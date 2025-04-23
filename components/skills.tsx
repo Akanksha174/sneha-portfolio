401b70caf445e067cd7a72c049a235a09625450f
Akanksha174 <akankshakumari4755@gmail.com>
2025-04-23 21:54:55 +0530

    Update skills.tsx

--- components/skills.tsx
+++ components/skills.tsx
@@ -1,34 +1,40 @@
-import React from 'react';
+"use client"
+
+import React from "react"
 
 const skills = [
-  { name: "Python", icon: "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg" },
+  { name: "VS Code", icon: "https://raw.githubusercontent.com/devicons/devicon/master/icons/vscode/vscode-original.svg" },
+  { name: "Figma", icon: "https://raw.githubusercontent.com/devicons/devicon/master/icons/figma/figma-original.svg" },
   { name: "Java", icon: "https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg" },
+  { name: "Python", icon: "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg" },
   { name: "C++", icon: "https://raw.githubusercontent.com/devicons/devicon/master/icons/cplusplus/cplusplus-original.svg" },
   { name: "C", icon: "https://raw.githubusercontent.com/devicons/devicon/master/icons/c/c-original.svg" },
-  { name: "R", icon: "https://raw.githubusercontent.com/devicons/devicon/master/icons/r/r-original.svg" },
+  { name: "R", icon: "https://www.r-project.org/logo/Rlogo.png" },
+  { name: "ggplot", icon: "https://upload.wikimedia.org/wikipedia/commons/0/05/Ggplot2_logo.png" },
+  { name: "SQL", icon: "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original.svg" },
+  { name: "MongoDB", icon: "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original.svg" },
+  { name: "Tableau", icon: "https://cdn.worldvectorlogo.com/logos/tableau-software.svg" },
+  { name: "Excel", icon: "https://img.icons8.com/color/48/000000/microsoft-excel-2019--v1.png" },
+  { name: "Power BI", icon: "https://upload.wikimedia.org/wikipedia/commons/c/cf/New_Power_BI_Logo.svg" },
+  { name: "Canva", icon: "https://img.icons8.com/color/48/000000/canva.png" },
   { name: "Git", icon: "https://raw.githubusercontent.com/devicons/devicon/master/icons/git/git-original.svg" },
   { name: "GitHub", icon: "https://raw.githubusercontent.com/devicons/devicon/master/icons/github/github-original.svg" },
-  { name: "SQL", icon: "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original.svg" },
-  { name: "MySQL", icon: "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg" },
-  { name: "VS Code", icon: "https://raw.githubusercontent.com/devicons/devicon/master/icons/vscode/vscode-original.svg" },
-  { name: "Canva", icon: "https://img.icons8.com/color/48/000000/canva.png" },
-  { name: "Tableau", icon: "https://img.icons8.com/color/48/000000/tableau-software.png" },
-  { name: "Excel", icon: "https://img.icons8.com/color/48/000000/microsoft-excel-2019--v1.png" },
-  { name: "Figma", icon: "https://raw.githubusercontent.com/devicons/devicon/master/icons/figma/figma-original.svg" },
-];
+  { name: "Vercel", icon: "https://raw.githubusercontent.com/devicons/devicon/master/icons/vercel/vercel-original.svg" },
+]
 
-export const Skills = () => {
+
+const Skills = () => {
   return (
     <section className="py-20 bg-gray-50">
       <div className="container mx-auto px-6">
         <h2 className="text-4xl font-bold mb-12 text-center">Skills</h2>
-        <div className="grid grid-cols-6 sm:grid-cols-6 gap-4 sm:gap-6">
+        <div className="grid grid-cols-2 sm:grid-cols-4 md:grid-cols-9 gap-10 place-items-center">
           {skills.map((skill, index) => (
-            <div key={index} className="flex flex-col items-center gap-1 sm:gap-1">
-              <img 
-                src={skill.icon} 
+            <div key={index} className="flex flex-col items-center">
+              <img
+                src={skill.icon}
                 alt={skill.name}
-                className="w-12 h-12 object-contain"
+                className="w-16 h-16 object-contain mb-2"
               />
               <span className="text-sm font-medium text-gray-700 text-center">{skill.name}</span>
             </div>
@@ -36,7 +42,7 @@ export const Skills = () => {
         </div>
       </div>
     </section>
-  );
-};
+  )
+}
 
-export default Skills;
+export default Skills
